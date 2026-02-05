2860d43e8eb3c309f9c9072d6ba8f2549382cb89
HarshDubey79 <harshdubey9597@gmail.com>
2026-02-05 23:48:46 +0530

    E-commerce site

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className='text-danger'>Welcome to Full Stack E-commerce Application</h1>
+     </>  
+  );
+}
+
+export default App;
