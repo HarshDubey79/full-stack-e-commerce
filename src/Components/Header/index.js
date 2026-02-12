70621044740dc139816ec641baed9a76368e732d
HarshDubey79 <harshdubey9597@gmail.com>
2026-02-12 23:25:51 +0530

    Style

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,4 +1,6 @@
+ import Logo from '../../assets/images/Main logo.png';
 
+import { Link } from "react-router-dom";
 
   const Header =()=> {
     return (
@@ -6,7 +8,16 @@
         <div className="headerwapper">
             <div className="top-strip bg-color">
                 <div className="container">
-                    <p className="mb-0 mt-0 text-center text-white">latest offers: free shipping on orders over $100</p>
+                    <p className="mb-0 mt-0 text-center">latest offers: free shipping on orders over <b>₹100</b></p>
+                </div>
+            </div>
+            <div className="header">
+                <div className="container">
+                    <div className="row">
+                        <div className="logoWrapper d-flex align-items-center col-sm-2">
+                            <Link to={'/'}><img src={Logo} alt='Logo'/></Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
